0a090cfb2bb42d693ffb68e20e7f47df5ed76e3a
chgibb <chris.gibb@outlook.com>
2016-10-25 09:41:01 -0400

    Fixed typo.

--- src/tagApplicatorTS/build.sh
+++ src/tagApplicatorTS/build.sh
@@ -2,7 +2,7 @@ rm -rf dist
 mkdir dist
 printf "Building tag applicator\n"
 ./node_modules/.bin/tsc
-printf "Bundling bundling tag applicator\n"
+printf "Bundling tag applicator\n"
 ./node_modules/.bin/browserify index.js --node -o dist/dist.js
 
 rm *.js
