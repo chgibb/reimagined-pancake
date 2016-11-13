c9b5aab1b466b9920060e978429c72915cb19d6f
chgibb <chris.gibb@outlook.com>
2016-11-12 23:23:02 -0500

    Removed siteClient

--- src/siteClient/build.sh
+++
@@ -1,9 +0,0 @@
-rm -rf dist
-mkdir dist
-printf "Building site client\n"
-./node_modules/.bin/tsc
-printf "Bundling site client\n"
-./node_modules/.bin/browserify index.js --debug -o  dist/dist.js
-
-rm *.js
-rm req/*.js
