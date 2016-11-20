0a0befe058a9acb6dd3c875d8126ea2b33af62be
chgibb <chris.gibb@outlook.com>
2016-11-20 15:09:20 -0500

    Modify averageData to stream output from queryBins

--- src/averageData/index.ts
+++ src/averageData/index.ts
@@ -1,10 +1,12 @@
 var argv : any = require('minimist')(process.argv.slice(2));
 
+let ndJson : any = require("ndjson");
+
 import * as fs from "fs";
 
 var jsonFile = require('./../jsreq/jsonfile');
 var canRead = require('./../jsreq/canRead');
-
+var assert = require('./../jsreq/assert');
 import * as tag from "./req/tagExists";
 
 var filePath = argv.file;
@@ -15,47 +17,61 @@ if(!filePath)
     process.exit(1);
 }
 //expect output from queryBins
-/*var file : Array<any> = jsonFile.readFileSync(filePath);
-if(!file)
+//var file : Array<any> = jsonFile.readFileSync(filePath);
+if(!canRead(filePath))
 {
     console.log("Error accessing "+filePath);
     process.exit(1);
-}*/
+}
 
 var nerTags : Array<tag.nerTagAverage> = new Array<tag.nerTagAverage>();
 var sentimentAverage : number = 0;
 var totalTweets = 0;
-
-/*for(let i = 0; i != file.length; ++i)
-{
-    if(file[i].nerTags)
+assert.runningEvents += 1;
+let stream = fs.createReadStream(filePath);
+stream.pipe(ndJson.parse()).on
+(
+    'data',(data : any) => 
     {
-        for(let k = 0; k != file[i].nerTags.length; ++k)
+        if(data.nerTags)
         {
-            tag.considerNerTag(nerTags,file[i].nerTags[k],file[i].sentiment);
+            for(let k = 0; k != data.nerTags.length; ++k)
+            {
+                tag.considerNerTag(nerTags,data.nerTags[k],data.sentiment);
+            }
         }
+        if(data.sentiment != undefined)
+        {
+            sentimentAverage += data.sentiment;
+        }
+        totalTweets += 1;
     }
-    if(file[i].sentiment != undefined)
+).on
+(
+    'end',() => 
     {
-        sentimentAverage += file[i].sentiment;
+        assert.runningEvents -= 1;
     }
-}*/
+);
 
-for(let i = 0; i != nerTags.length; ++i)
-{
-    nerTags[i].sentimentAverage = nerTags[i].sentimentAverage/nerTags[i].count;
-}
+assert.assert
+(
+    ()=>
+    {
+        for(let i = 0; i != nerTags.length; ++i)
+        {
+            nerTags[i].sentimentAverage = nerTags[i].sentimentAverage/nerTags[i].count;
+        }
 
-var res : any = 
-{
-    nerTags : nerTags,
-    sentimentAverage : sentimentAverage/file.length,
-    date : date,
-    totalTweets : file.length
-}
-
-
-
-console.log(JSON.stringify(res,undefined,4));
-
-
+        var res : any = 
+        {
+            nerTags : nerTags,
+            sentimentAverage : sentimentAverage/totalTweets,
+            date : date,
+            totalTweets : totalTweets
+        }
+        console.log(JSON.stringify(res,undefined,4));
+        return true;
+    },'',0
+);
+assert.runAsserts();
