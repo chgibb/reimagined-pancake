d20d0d725e8f91b4660f704c1cd4be5ab06b304a
chgibb <chris.gibb@outlook.com>
2016-12-30 16:13:05 -0500

    Add error and bash handling

--- build.bash
+++ build.bash
@@ -1,11 +1,20 @@
 #!/bin/bash
 sh clean.sh
 ./node_modules/.bin/tsc
+if [ $? != 0 ]; then
+    exit 1
+fi
 cd src
 for d in */ ; do
     cd $d
     if [ -f "build.sh" ]; then
         sh build.sh
+    fi
+    if [ -f "build.bash" ]; then
+        bash build.bash
+    fi
+    if [ $? != 0 ]; then
+        exit 1
     fi
     cd ../
 done
