5ceba8c086f8c37b072c50d0e7fa2779c48ed7fb
chgibb <chris.gibb@outlook.com>
2017-01-02 14:02:12 -0500

    Move things into before install. Fix formatting

--- src/binDiscoverer/build.bash
+++ src/binDiscoverer/build.bash
@@ -1,7 +1,7 @@
 rm -rf dist
 mkdir dist
 printf "Building binDiscoverer\n"
-g++ -Wall -fexceptions -fexpensive-optimizations -O3 -std=c++11  -c main.cpp -o main.o
+g++ -Wall -fexceptions -fexpensive-optimizations -O3 -std=c++11 -c main.cpp -o main.o
 if [ $? != 0 ]; then
     exit 1
 fi
