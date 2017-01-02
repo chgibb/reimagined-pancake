324ff66cdd9221164399caa403d48242ced4bb19
chgibb <chris.gibb@outlook.com>
2017-01-02 14:52:59 -0500

    Remove failure checks from tagApplicator build

--- src/tagApplicator/build.bash
+++ src/tagApplicator/build.bash
@@ -2,10 +2,6 @@ rm -rf dist
 mkdir dist
 printf "Building tagApplicator\n"
 g++ -Wall -fexceptions -fexpensive-optimizations -O3 -std=c++11  -c main.cpp -o main.o
-if [ $? != 0 ]; then
-    exit 1
-fi
+
 g++  -o dist/tagApplicator main.o  -s  
-if [ $? != 0 ]; then
-    exit 1
-fi
+
