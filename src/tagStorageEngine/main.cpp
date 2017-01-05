28cb6c919fa8eb418405f0f3fde55e02f92adc22
chgibb <chris.gibb@outlook.com>
2017-01-05 18:58:51 -0500

    Wrap function with frame creation/deletion

--- src/tagStorageEngine/main.cpp
+++ src/tagStorageEngine/main.cpp
@@ -4,8 +4,10 @@ extern "C"
 {
     JNIEXPORT void JNICALL Java_src_TagStorageEngine_storeTag(JNIEnv* env,jobject obj,jstring token,jstring entity)
     {
+        env->PushLocalFrame(2);
         std::string nToken(env->GetStringUTFChars(token,NULL));
         std::string nEntity(env->GetStringUTFChars(entity,NULL));
+        env->PopLocalFrame(NULL);
     }
     JNIEXPORT void JNICALL Java_src_TagStorageEngine_setStorageDirectory(JNIEnv* env,jobject obj,jstring dir)
     {
