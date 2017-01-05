d21ab3939f214fc7bd1213938e67d0e75ed03917
chgibb <chris.gibb@outlook.com>
2017-01-05 18:47:49 -0500

    Remove artifacts

--- src/tagStorageEngine/main.cpp
+++ src/tagStorageEngine/main.cpp
@@ -6,29 +6,9 @@ extern "C"
     {
         std::string nToken(env->GetStringUTFChars(token,NULL));
         std::string nEntity(env->GetStringUTFChars(entity,NULL));
-        //std::cout<<nToken<<"\n";
-        //std::cout<<nEntity<<"\n";
-        //env->ReleaseStringUTFChars(env,token);
-        //env->ReleaseStringUTFChars(env,entity);
     }
     JNIEXPORT void JNICALL Java_src_TagStorageEngine_setStorageDirectory(JNIEnv* env,jobject obj,jstring dir)
     {
         std::cout<<dir;
-    }
-    
-    // A function adding two integers and returning the result
-    int SampleAddInt(int i1, int i2)
-    {
-        return i1 + i2;
-    }
-
-
-
-    // A function always returning zero
-    int SampleFunction2()
-    {
-        // insert code here
-        
-        return 0;
     }
 }
