b6b885dac52d7da93acc81956f150b7e2117ea2a
chgibb <chris.gibb@outlook.com>
2017-01-16 18:59:19 -0500

    Scaffold jni backend for GetRawTweets class

---
+++ src/getRawTweets/build.sh
@@ -0,0 +1,16 @@
+#TODO: THIS PATH SHOULD NOT BE HARDCODED
+rm -rf dist
+mkdir dist
+#g++ -Wall -fexceptions -O3 -fPIC -I /usr/lib/jvm/java-7-openjdk-amd64/include  -c main.cpp -o main.o
+#g++ -shared  main.o  -o libtagStorageEngine.so -s  
+
+g++ -shared -fPIC -std=c++11 -I /usr/lib/jvm/java-7-openjdk-amd64/include main.cpp -o libgetRawTweets.so
+printf "Building libgetRawTweets.so\n"
+if [ $? != 0 ]; then
+    exit 1
+fi
+
+
+
+cp libgetRawTweets.so dist
+
---
+++ src/getRawTweets/clean.sh
@@ -0,0 +1,3 @@
+rm *.so
+rm *.o
+rm -rf learned
