74bccd7d78b97352c8cae9c67e5fc73e15246522
chgibb <chris.gibb@outlook.com>
2017-01-16 19:50:27 -0500

    Scaffold c++ endpoints for java class binding

---
+++ src/getTweetsFromBin/main.cpp
@@ -0,0 +1,13 @@
+#include <jni.h>
+#include "../inc/getTweetsFromBin.hpp"
+extern "C"
+{
+    JNIEXPORT void JNICALL Java_src_GetTweetsFromBin_loadBin(JNIEnv*env,jobject obj,jstring bin)
+    {
+
+    }
+    JNIEXPORT jstring JNICALL Java_src_GetTweetsFromBin_getNextTweet(JNIEnv*env,jobject obj)
+    {
+        
+    }
+}
