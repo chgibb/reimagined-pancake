868920679cca0e22968044145396e746695956d8
chgibb <chris.gibb@outlook.com>
2017-01-18 16:53:38 -0500

    Move init logic from input queue into main class

--- src/nerServer/nerServer.java
+++ src/nerServer/nerServer.java
@@ -11,19 +11,40 @@ import edu.stanford.nlp.util.StringUtils;
 
 import java.io.IOException;
 import java.util.Scanner;
+import java.util.concurrent.*;
+import java.util.ArrayList;
+import java.lang.Thread;
 
 import src.*;
 
 public class nerServer
 {
+    public static TagStorageEngine tagEngine;
+    public static SlashTagParser slashTagParser;
+    public static AbstractSequenceClassifier classifier;
     public static void main(String[] args) throws IOException 
     {
-        Scanner scanner = new Scanner(System.in);
-        inputQueue queue = new inputQueue(args);
-        queue.start();
-        for(;;)
+        String learnedClassifierDirectory = "";
+        String trainedClassifier = "";
+        for(int i = 0; i != args.length; ++i)
         {
-            queue.queue.add(scanner.nextLine());
+            String[] split = args[i].split("=");
+            for(int k = 0; k != split.length; ++k)
+            {
+                if(split[k].equals("--learnedClassifierDirectory"))
+                {
+                   learnedClassifierDirectory = split[k+1];
+                }
+                if(split[k].equals("--trainedClassifier"))
+                {
+                    trainedClassifier = split[k+1];
+                }
+            }
         }
+        tagEngine = new TagStorageEngine();
+        tagEngine.setStorageDirectory(learnedClassifierDirectory);
+        slashTagParser = new SlashTagParser();
+        String serializedClassifier = trainedClassifier;
+        classifier = CRFClassifier.getClassifierNoExceptions(serializedClassifier);
     }
 }
