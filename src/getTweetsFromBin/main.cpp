a0fd7a2b01cd9c343c6f05de9e7671b91ceb4a6a
chgibb <chris.gibb@outlook.com>
2017-01-18 19:49:40 -0500

    Update java backend binding for GetTweetsFromBin class

--- src/getTweetsFromBin/main.cpp
+++ src/getTweetsFromBin/main.cpp
@@ -1,12 +1,21 @@
 #include <jni.h>
 #include "../inc/getTweetsFromBin.hpp"
+::GetTweetsFromBin getTweets;
 extern "C"
 {
-    JNIEXPORT void JNICALL Java_src_GetTweetsFromBin_loadBin(JNIEnv*env,jobject obj,jstring bin)
+    JNIEXPORT jboolean JNICALL Java_src_GetTweetsFromBin_loadBin(JNIEnv*env,jobject obj,jstring bin)
     {
 
     }
-    JNIEXPORT jstring JNICALL Java_src_GetTweetsFromBin_getNextTweet(JNIEnv*env,jobject obj)
+    JNIEXPORT void JNICALL Java_src_GetTweetsFromBin_clearBin(JNIEnv*env,jobject obj)
+    {
+        
+    }
+    JNIEXPORT jstring JNICALL Java_src_GetTweetsFromBin_getTweet(JNIEnv*env,jobject obj)
+    {
+        
+    }
+    JNIEXPORT void JNICALL Java_src_GetTweetsFromBin_pop(JNIEnv*env,jobject obj)
     {
         
     }
