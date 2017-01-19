ed61443053f88dad6604b92bca2cc2f6ffc56ac3
chgibb <chris.gibb@outlook.com>
2017-01-19 13:34:17 -0500

    Add method bodies to C++ end point

--- src/getTweetsFromBin/main.cpp
+++ src/getTweetsFromBin/main.cpp
@@ -5,18 +5,29 @@ extern "C"
 {
     JNIEXPORT jboolean JNICALL Java_src_GetTweetsFromBin_loadBin(JNIEnv*env,jobject obj,jstring bin)
     {
-
+        env->PushLocalFrame(1);
+        bool res = getTweets.loadBin(env->GetStringUTFChars(bin,NULL));
+        if(res)
+        {
+            env->PopLocalFrame(NULL);
+            return JNI_TRUE;
+        }
+        else
+        {
+            env->PopLocalFrame(NULL);
+            return JNI_FALSE;
+        }
     }
     JNIEXPORT void JNICALL Java_src_GetTweetsFromBin_clearBin(JNIEnv*env,jobject obj)
     {
-        
+        getTweets.clearBin();
     }
     JNIEXPORT jstring JNICALL Java_src_GetTweetsFromBin_getTweet(JNIEnv*env,jobject obj)
     {
-        
+        return env->NewStringUTF(getTweets.getTweet().c_str());
     }
     JNIEXPORT void JNICALL Java_src_GetTweetsFromBin_pop(JNIEnv*env,jobject obj)
     {
-        
+        getTweets.pop();
     }
 }
