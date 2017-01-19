7bb220224777d5d5ffad3516f125e6439906e14c
chgibb <chris.gibb@outlook.com>
2017-01-19 13:41:48 -0500

    Fix output .so name

--- src/getTweetsFromBin/build.sh
+++ src/getTweetsFromBin/build.sh
@@ -4,8 +4,8 @@ mkdir dist
 #g++ -Wall -fexceptions -O3 -fPIC -I /usr/lib/jvm/java-7-openjdk-amd64/include  -c main.cpp -o main.o
 #g++ -shared  main.o  -o libtagStorageEngine.so -s  
 
-g++ -shared -fPIC -std=c++11 -I /usr/lib/jvm/java-7-openjdk-amd64/include main.cpp -o libgetRawTweets.so
-printf "Building libgetRawTweets.so\n"
+g++ -shared -fPIC -std=c++11 -I /usr/lib/jvm/java-7-openjdk-amd64/include main.cpp -o libgetTweetsFromBin.so
+printf "Building libgetTweetsFromBin.so\n"
 if [ $? != 0 ]; then
     exit 1
 fi
