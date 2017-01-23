5f1313ebc991aa8490a1024ed75c0ee991f23396
chgibb <chris.gibb@outlook.com>
2017-01-23 17:49:04 -0500

    Change gitignore and ner wrapper script to reflect name change

---
+++ res/runNerLearner.sh
@@ -0,0 +1,1 @@
+java -jar nerLearner.jar --trainedClassifier=classifiers/ner-eng-ie.crf-3-all2008.ser.gz --learnedClassifierDirectory=classifiers/learned --binList=$1
--- res/runNerServer.sh
+++
@@ -1,1 +0,0 @@
-java -jar nerServer.jar --trainedClassifier=classifiers/ner-eng-ie.crf-3-all2008.ser.gz --learnedClassifierDirectory=classifiers/learned --binList=$1
