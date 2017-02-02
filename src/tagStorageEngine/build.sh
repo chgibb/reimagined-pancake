95770e60a5d1296336dce54e91ebfc6aa17f25db
Chris Gibb <chris.gibb@outlook.com>
2017-02-02 12:31:48 -0500

    Change OpenJDK version to 9

--- src/tagStorageEngine/build.sh
+++ src/tagStorageEngine/build.sh
@@ -4,7 +4,7 @@ mkdir dist
 #g++ -Wall -fexceptions -O3 -fPIC -I /usr/lib/jvm/java-7-openjdk-amd64/include  -c main.cpp -o main.o
 #g++ -shared  main.o  -o libtagStorageEngine.so -s  
 
-g++ -shared -fPIC -std=c++11 -I /usr/lib/jvm/java-7-openjdk-amd64/include main.cpp -o libtagStorageEngine.so
+g++ -shared -fPIC -std=c++11 -I /usr/lib/jvm/java-9-openjdk-amd64/include main.cpp -o libtagStorageEngine.so
 printf "Building tagStorageEnginge\n"
 if [ $? != 0 ]; then
     exit 1
