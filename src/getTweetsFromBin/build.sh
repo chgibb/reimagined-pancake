b62397edaf1da613fdc047fe56ac2570e1775236
Chris Gibb <chris.gibb@outlook.com>
2017-02-02 12:32:56 -0500

    Removed commented out paths

--- src/getTweetsFromBin/build.sh
+++ src/getTweetsFromBin/build.sh
@@ -1,8 +1,6 @@
 #TODO: THIS PATH SHOULD NOT BE HARDCODED
 rm -rf dist
 mkdir dist
-#g++ -Wall -fexceptions -O3 -fPIC -I /usr/lib/jvm/java-7-openjdk-amd64/include  -c main.cpp -o main.o
-#g++ -shared  main.o  -o libtagStorageEngine.so -s  
 
 g++ -shared -fPIC -std=c++11 -I /usr/lib/jvm/java-9-openjdk-amd64/include main.cpp -o libgetTweetsFromBin.so
 printf "Building libgetTweetsFromBin.so\n"
