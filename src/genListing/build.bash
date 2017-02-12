b8d7d40d9f30c564e271d0ca657b0807ae40e28a
Chris Gibb <chris.gibb@outlook.com>
2017-02-12 15:03:57 -0500

    Rename binDiscoverer to genListing

--- src/genListing/build.bash
+++ src/genListing/build.bash
@@ -1,11 +1,11 @@
 rm -rf dist
 mkdir dist
-printf "Building binDiscoverer\n"
+printf "Building genListing\n"
 g++ -Wall -fexceptions -fexpensive-optimizations -O3 -std=c++11 -c main.cpp -o main.o
 if [ $? != 0 ]; then
     exit 1
 fi
-g++  -o dist/binDiscoverer main.o  -s  
+g++  -o dist/genListing main.o  -s  
 if [ $? != 0 ]; then
     exit 1
 fi
