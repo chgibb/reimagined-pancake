3bdc76ea30e0b44684c5b1a460fb5a41d950cf50
Chris Gibb <chris.gibb@outlook.com>
2017-02-12 15:12:43 -0500

    Get data dir directly from cli instead of going through minimist

--- src/genListingNoDate/index.ts
+++ src/genListingNoDate/index.ts
@@ -1,3 +1,10 @@
 import * as fs from "fs";
 
 let walk = require("walk");
+
+let dataDir = process.argv[2];
+if(!dataDir)
+{
+    console.log("Must specify directory to generate listing for!");
+    process.exit(1);
+}
