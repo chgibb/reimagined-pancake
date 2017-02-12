669871691dbea99b18e9c9f9a791b0fb4baee312
Chris Gibb <chris.gibb@outlook.com>
2017-02-12 15:18:03 -0500

    Scaffold basic example

--- src/genListingNoDate/index.ts
+++ src/genListingNoDate/index.ts
@@ -8,3 +8,14 @@ if(!dataDir)
     console.log("Must specify directory to generate listing for!");
     process.exit(1);
 }
+
+let walker = walk.walk(dataDir,{});
+
+walker.on
+(
+    "file",(root : string,fileStats : any, next : () => void) =>
+    {
+        console.log(fileStats.name);
+        next();
+    }
+);
