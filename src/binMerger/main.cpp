6efaf6a818ddeff9d7ba77f75dc87f8a15dddb27
Chris Gibb <chris.gibb@outlook.com>
2017-02-17 11:16:53 -0500

    Scaffold getting source bin paths

--- src/binMerger/main.cpp
+++ src/binMerger/main.cpp
@@ -1,6 +1,25 @@
-
+#include <string>
+#include <fstream>
+#include <vector>
+#include <iostream>
 using namespace std;
 int main(int argc,char*argv[])
 {
+    if(argc < 4)
+    {
+        std::cout<<"Invalid number of arguments!\n";
+        return 1;
+    }
+    std::string srcDirName = argv[1];
+    std::string destDirName = argv[2];
+    std::string srcBinList = argv[3];
+
+    std::ifstream srcBinListing(srcBinList.c_str(),std::ios::in);
+    std::string bin = "";
+    while(std::getline(srcBinListing,bin))
+    {
+        std::cout<<bin<<std::endl;
+    }
+
     return 0;
 }
