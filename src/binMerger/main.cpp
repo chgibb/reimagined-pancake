f6066f0b41b482b4b1438ed20709163d5891eeeb
Chris Gibb <chris.gibb@outlook.com>
2017-02-17 12:35:36 -0500

    Implemented hash fetching and duplicate finding

--- src/binMerger/main.cpp
+++ src/binMerger/main.cpp
@@ -10,6 +10,23 @@
 #include "rapidjson/include/rapidjson/istreamwrapper.h"
 #include "rapidjson/include/rapidjson/ostreamwrapper.h"
 #include "rapidjson/include/rapidjson/prettywriter.h"
+
+void getHashes(const char*file,std::vector<std::string>&hashes)
+{
+    std::ifstream*bin = new std::ifstream(file,std::ios::in);
+    ::getQuotedJSONProperty<decltype(bin)>
+    (
+        bin,
+        "textHash",
+        [&hashes](std::string&textHash) -> bool
+        {
+            hashes.push_back(textHash);
+            return false;
+        }
+    );
+    delete bin;
+}
+
 using namespace std;
 int main(int argc,char*argv[])
 {
@@ -30,8 +47,24 @@ int main(int argc,char*argv[])
     {
         std::string srcBin = bin;
         std::string destBin = std::regex_replace(bin,srcDirRegex,destDirName);
-        std::cout<<destBin<<std::endl;
-    }
 
+        std::vector<std::string> srcBinHashes;
+        std::vector<std::string> destBinHashes;
+        std::vector<std::string> dupHashes;
+
+        ::getHashes(srcBin.c_str(),srcBinHashes);
+        ::getHashes(destBin.c_str(),destBinHashes);
+
+        auto srcEnd = srcBinHashes.end();
+        auto destEnd = destBinHashes.end();
+        for(auto srcIt = srcBinHashes.begin(); srcIt != srcEnd; ++srcIt)
+        {
+            for(auto destIt = destBinHashes.begin(); destIt != destEnd; ++destIt)
+            {
+                if(*srcIt == *destIt)
+                    dupHashes.push_back(*srcIt);
+            }
+        }
+    }
     return 0;
 }
