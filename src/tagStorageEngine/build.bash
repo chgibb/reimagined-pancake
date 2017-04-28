d1d8c9c065f83f07c859a24381bcca1622924f7f
Chris Gibb <chris.gibb@outlook.com>
2017-04-27 22:14:45 -0400

    Fallback from OpenJDK 9 through 7

--- src/tagStorageEngine/build.bash
+++ src/tagStorageEngine/build.bash
@@ -10,7 +10,13 @@ fi
 
 $CXX -shared -fPIC -std=c++11 -I /usr/lib/jvm/java-9-openjdk-amd64/include main.cpp -o libtagStorageEngine.so
 if [ $? != 0 ]; then
-    exit 1
+    $CXX -shared -fPIC -std=c++11 -I /usr/lib/jvm/java-8-openjdk-amd64/include main.cpp -o libtagStorageEngine.so
+    if [ $? != 0 ]; then
+        $CXX -shared -fPIC -std=c++11 -I /usr/lib/jvm/java-7-openjdk-amd64/include main.cpp -o libtagStorageEngine.so
+        if [ $? != 0 ]; then
+            exit 1
+        fi
+    fi
 fi
 
 cp libtagStorageEngine.so dist
