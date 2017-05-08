7a4df75207d3f56e06b8cd79a5106b7e9d6bc452
chgibb <chris.gibb@outlook.com>
2017-05-08 16:14:32 -0400

    Wrapper for win32

--- src/getTweetsFromBin/build.bash
+++ src/getTweetsFromBin/build.bash
@@ -9,16 +9,21 @@ CXX="g++"
 if [ "$TRAVIS" = true ]; then 
     CXX="/usr/bin/g++-5"
 fi
-
-$CXX -shared -fPIC -std=c++11 -I /usr/lib/jvm/java-9-openjdk-amd64/include main.cpp -o libgetTweetsFromBin.so
-if [ $? != 0 ]; then
-    $CXX -shared -fPIC -std=c++11 -I /usr/lib/jvm/java-8-openjdk-amd64/include main.cpp -o libgetTweetsFromBin.so
+if [[ "$OSTYPE" == "linux-gnu" ]]; then
+    $CXX -shared -fPIC -std=c++11 -I /usr/lib/jvm/java-9-openjdk-amd64/include main.cpp -o libgetTweetsFromBin.so
     if [ $? != 0 ]; then
-        $CXX -shared -fPIC -std=c++11 -I /usr/lib/jvm/java-7-openjdk-amd64/include main.cpp -o libgetTweetsFromBin.so
+        $CXX -shared -fPIC -std=c++11 -I /usr/lib/jvm/java-8-openjdk-amd64/include main.cpp -o libgetTweetsFromBin.so
         if [ $? != 0 ]; then
-            exit 1
+            $CXX -shared -fPIC -std=c++11 -I /usr/lib/jvm/java-7-openjdk-amd64/include main.cpp -o libgetTweetsFromBin.so
+            if [ $? != 0 ]; then
+                exit 1
+            fi
         fi
     fi
+fi
+
+if [[ "$OSTYPE" == "cygwin" ]]; then
+    $CXX -shared -std=c++11 -O3 -D__int64=int64_t -I "$JAVA_HOME"\\include -I "$JAVA_HOME"\\include\\win32 main.cpp -o libgetTweetsFromBin.dll
 fi
 
 cp libgetTweetsFromBin.so dist
