3fa22a0918e3eb1260c882fb7074dacc1cd600ac
chgibb <chris.gibb@outlook.com>
2017-05-08 16:22:57 -0400

    Fixed build for windows

--- src/tagStorageEngine/build.bash
+++ src/tagStorageEngine/build.bash
@@ -10,15 +10,26 @@ if [ "$TRAVIS" = true ]; then
     CXX="/usr/bin/g++-5"
 fi
 
-$CXX -shared -fPIC -std=c++11 -I /usr/lib/jvm/java-9-openjdk-amd64/include main.cpp -o libtagStorageEngine.so
-if [ $? != 0 ]; then
-    $CXX -shared -fPIC -std=c++11 -I /usr/lib/jvm/java-8-openjdk-amd64/include main.cpp -o libtagStorageEngine.so
+if [[ "$OSTYPE" == "linux-gnu" ]]; then
+    $CXX -shared -fPIC -std=c++11 -O3 -I /usr/lib/jvm/java-9-openjdk-amd64/include main.cpp -o libtagStorageEngine.so
     if [ $? != 0 ]; then
-        $CXX -shared -fPIC -std=c++11 -I /usr/lib/jvm/java-7-openjdk-amd64/include main.cpp -o libtagStorageEngine.so
+        $CXX -shared -fPIC -std=c++11 -O3 -I /usr/lib/jvm/java-8-openjdk-amd64/include main.cpp -o libtagStorageEngine.so
         if [ $? != 0 ]; then
-            exit 1
+            $CXX -shared -fPIC -std=c++11 -O3 -I /usr/lib/jvm/java-7-openjdk-amd64/include main.cpp -o libtagStorageEngine.so
+            if [ $? != 0 ]; then
+                exit 1
+            fi
         fi
     fi
+    cp libtagStorageEngine.so dist
+    rm *.so
 fi
 
-cp libtagStorageEngine.so dist
+if [[ "$OSTYPE" == "cygwin" ]]; then
+$CXX -shared -std=c++11 -O3 -D__int64=int64_t -I "$JAVA_HOME"\\include -I "$JAVA_HOME"\\include\\win32 main.cpp -o libtagStorageEngine.dll
+    if [ $? != 0 ]; then
+        exit 1
+    fi
+    cp libtagStorageEngine.dll dist
+    rm *.dll
+fi
