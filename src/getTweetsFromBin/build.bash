159b303f0639d75efd99d07474a33070f7214a53
chgibb <chris.gibb@outlook.com>
2017-05-08 16:17:33 -0400

    Added error cehcking and artifact copying

--- src/getTweetsFromBin/build.bash
+++ src/getTweetsFromBin/build.bash
@@ -20,12 +20,18 @@ if [[ "$OSTYPE" == "linux-gnu" ]]; then
             fi
         fi
     fi
+    cp libgetTweetsFromBin.so dist
+    rm *.so
 fi
 
 if [[ "$OSTYPE" == "cygwin" ]]; then
     $CXX -shared -std=c++11 -O3 -D__int64=int64_t -I "$JAVA_HOME"\\include -I "$JAVA_HOME"\\include\\win32 main.cpp -o libgetTweetsFromBin.dll
+    if [ $? != 0 ]; then
+        exit 1
+    fi
+    cp libgetTweetsFromBin.dll dist
+    rm *.dll
 fi
 
-cp libgetTweetsFromBin.so dist
-rm *.so
+
 
