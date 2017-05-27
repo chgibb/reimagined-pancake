74cad169381b78bc132e667daa992d2df09f2c69
Chris Gibb <chris.gibb@outlook.com>
2017-05-27 10:02:46 -0400

    Created twitterScraper module

---
+++ src/twitterScraper/build.bash
@@ -0,0 +1,10 @@
+(set -o igncr) 2>/dev/null && set -o igncr; # For Cygwin on Windows compaibility
+
+rm -rf dist
+mkdir dist
+
+printf "Bundling twitter miner\n"
+./../../node_modules/.bin/browserify index.js --node -o dist/dist.js
+if [ $? != 0 ]; then
+    exit 1
+fi
