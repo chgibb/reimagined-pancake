6cb80cadd175ce5f4d1d6cf20b9567007dcbcdf0
Chris Gibb <chris.gibb@outlook.com>
2017-05-27 23:30:48 -0400

    Removed unused code

--- src/twitterScraper/index.ts
+++ src/twitterScraper/index.ts
@@ -7,50 +7,12 @@ const readLines = require("n-readlines");
 
 let argv = require("minimist")(process.argv.slice(2));
 
-import {default as Tweet} from "./../req/tweet";
-import decomposedTweetDate from "./../req/decomposedTweetDate";
-import dataStore from "./../req/dataStore";
-import {ScrapedTweetAnalyzer} from "./req/scrapedTweetAnalyzer";
-import tweetAnalyzer from './../req/tweetAnalyzer';
-
 import {mergeScrapedTweets} from "./req/mergeScrapedTweets";
 import {scrapeUser} from "./req/scrapeUser";
 
 const dataDir : string = argv.dataDir;
 
 const nerBucket : string = argv.nerBucket;
-
-
-
-let store : dataStore<Tweet,decomposedTweetDate> = new dataStore<Tweet,decomposedTweetDate>();
-
-
-
-function deDup(store : dataStore<Tweet,decomposedTweetDate>) : void
-{
-    for(let i : number = store.items.length - 1; i != -1; --i)
-    {
-        for(let k : number = 0; k != store.items.length - 1; ++k)
-        {
-            let l = store.items[i];
-            let r = store.items[k];
-            if(l.textHash == r.textHash &&
-            i != k &&
-            l.second == r.second &&
-            l.minute == r.minute &&
-            l.hour == r.hour &&
-            l.day == r.day &&
-            l.month == r.month &&
-            l.year == r.year)
-            {
-                console.log(store.items.splice(i,1));
-                
-            }
-        }
-    }
-}
-
-let tweetStream : Array<any> = new Array<any>();
 
 let nerBucketReadStream = new readLines(nerBucket);
 
@@ -60,8 +22,6 @@ function scrapeNextUser() : void
     let line = nerBucketReadStream.next();
     if(!line)
     {
-        //deDup(store);
-        //saveTweetsFromStore(tweetSaveMgr,store,dataDir);
         clearInterval(keepAlive);
         process.exitCode = 0;
         return;
